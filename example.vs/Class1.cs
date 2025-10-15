dbc7f03d00ae89e6ab5bb24f1c577f87bcd3d628
chaitaya kumar <bobbymarkapudi@gmail.com>
2025-10-15 19:46:56 +0530

    test1 and example is commeted

---
+++ example.vs/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace example
+{
+    public class Class1
+    {
+        public static void Main(string[] arg)
+        {
+            Console.WriteLine("this is project related to git");
+        }
+    }
+}
